d09ae3d6ad2f260894fa5c01d3e765c55565290e
tascoma <scomatony@outlook.com>
2023-04-07 21:35:47 -0500

    Cleaning up code

--- financialstatements/utils.py
+++ financialstatements/utils.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import os
+
+
+def creating_input_paths(directory):
+    files = os.listdir(directory)
+    paths = []
+    for file in files:
+        path = os.path.join(directory, file)
+        paths.append(path)
+    return paths
 
 
 def missing_gl_check(df):
@@ -15,3 +25,24 @@ def debit_credit_check(df):
     debit_total = df['DEBIT'].sum()
     credit_total = df['CREDIT'].sum()
     return debit_total == credit_total
+
+
+def creating_output(df, month_df, type, config, cwd):
+    df['Month_Num'] = df['Date'].dt.month
+    df['Transaction_ID'] = type + '-' + \
+        df['Month_Num'].astype("str") + '-' + (df.index + 1).astype("str")
+    df = pd.merge(df,
+                  month_df,
+                  on='Month_Num',
+                  how='left')
+    months = df['Month'].unique()
+    file_path = os.path.join(cwd, config.get(
+        "data_outputs_directory", "JOURNAL_ENTRIES"))
+    month_num = 1
+    for month in months:
+        file_df = df[df['Month'] == month]
+        file_df = file_df[['Transaction_ID', 'Date', 'GL_Code',
+                           'Account', 'Description', 'DEBIT', 'CREDIT']]
+        file_name = f'{month_num}_{type}_{month}_entries.csv'
+        file_df.to_csv(os.path.join(file_path, file_name), index=False)
+        month_num += 1
--- scripts/creditcard_etl.py
+++ scripts/creditcard_etl.py
@@ -3,9 +3,11 @@ import add_package
 import configparser
 import pandas as pd
 import numpy as np
+from financialstatements.utils import creating_input_paths
 from financialstatements.utils import missing_gl_check
 from financialstatements.utils import missing_mcc
 from financialstatements.utils import debit_credit_check
+from financialstatements.utils import creating_output
 
 cwd = os.path.dirname(__file__)
 
@@ -14,119 +16,91 @@ config = configparser.ConfigParser()
 config_path = os.path.join(cwd, "../docs/config.ini")
 config.read(config_path)
 
-# Reading creditcard data
+
+def creating_df(paths):
+    dfs = []
+    for path in paths:
+        df = pd.read_csv(path)
+        dfs.append(df)
+    return pd.concat(dfs).reset_index(drop=True)
+
+
+def joining_gl_codes(df, coa_mcc_df):
+    df['Amount'] = df['Amount'].abs()
+    df = df.pivot_table(index=['Date', 'Name', 'Memo'],
+                        columns='Transaction', values='Amount')
+    df = df.reset_index()
+    df[['Memo', 'MCC', 'Blank']] = df['Memo'].str.split(';', expand=True)
+    df['MCC'] = df['MCC'].str[-4:]
+    df['MCC'] = df['MCC'].astype('int')
+    df = pd.merge(df,
+                  coa_mcc_df,
+                  how='left',
+                  on='MCC')
+    return df
+
+
+def processing_df(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df['Description'] = df['MCC_Description'] + ': ' + df['Name']
+    df = df[['Date', 'GL_Code', 'Account', 'Description', 'DEBIT', 'CREDIT']]
+    df = df.round(2)
+    df = df.sort_values(by='Date').reset_index(drop=True)
+    df['Order_Col'] = df.index + 1
+    df['Sub_Order_Col'] = np.where(df['DEBIT'].isnull(), 2, 1)
+    return df
+
+
+def creating_credit_entries(df):
+    df = df[df['CREDIT'].isnull()].reset_index(drop=True).copy()
+    df['GL_Code'] = 200101
+    df['Account'] = 'EdwardJones MasterCard'
+    df['Sub_Order_Col'] = 2
+    df = df.rename(columns={'DEBIT': 'CREDIT', 'CREDIT': 'DEBIT'})
+    return df[['Date', 'GL_Code', 'Account', 'Description', 'DEBIT', 'CREDIT', 'Order_Col', 'Sub_Order_Col']]
+
+
+def creating_debit_entries(df):
+    df = df[df['DEBIT'].isnull()].reset_index(drop=True).copy()
+    df['GL_Code'] = 200101
+    df['Account'] = 'EdwardJones MasterCard'
+    df['Sub_Order_Col'] = 1
+    df = df.rename(columns={'DEBIT': 'CREDIT', 'CREDIT': 'DEBIT'})
+    return df[['Date', 'GL_Code', 'Account', 'Description', 'DEBIT', 'CREDIT', 'Order_Col', 'Sub_Order_Col']]
+
+
+# Reading data
 CREDITCARD_DIRECTORY = os.path.join(cwd, config.get(
     "data_inputs_directory", "CREDITCARD_DIRECTORY"))
-creditcard_files = os.listdir(CREDITCARD_DIRECTORY)
+paths = creating_input_paths(CREDITCARD_DIRECTORY)
+df = creating_df(paths)
 
-creditcard_paths = []
-for file in creditcard_files:
-    path = os.path.join(CREDITCARD_DIRECTORY, file)
-    creditcard_paths.append(path)
-
-dfs = []
-for path in creditcard_paths:
-    df = pd.read_csv(path)
-    dfs.append(df)
-
-creditcard_df = pd.concat(dfs).reset_index(drop=True)
-
-# Reading table data
-COA_DATA = config.get('table_files', 'COA_DATA')
-MONTH_DATA = config.get('table_files', 'MONTH_DATA')
-coa_mcc_df = pd.read_excel(os.path.join(
-    cwd, COA_DATA), sheet_name='coa_mcc_link_table')
-month_df = pd.read_excel(os.path.join(
-    cwd, MONTH_DATA))
+# Reading tables
+COA_DATA = os.path.join(cwd, config.get('table_files', 'COA_DATA'))
+MONTH_DATA = os.path.join(cwd, config.get('table_files', 'MONTH_DATA'))
+coa_mcc_df = pd.read_excel(COA_DATA, sheet_name='coa_mcc_link_table')
+month_df = pd.read_excel(MONTH_DATA)
 
 # ETL
-creditcard_df['Date'] = pd.to_datetime(creditcard_df['Date'])
-creditcard_df['Amount'] = creditcard_df['Amount'].abs()
-creditcard_df = creditcard_df.pivot_table(
-    index=['Date', 'Name', 'Memo'], columns='Transaction', values='Amount')
-creditcard_df = creditcard_df.reset_index()
-creditcard_df[['Memo', 'MCC', 'Blank']
-              ] = creditcard_df['Memo'].str.split(';', expand=True)
-creditcard_df['MCC'] = creditcard_df['MCC'].str[-4:]
-creditcard_df['MCC'] = creditcard_df['MCC'].astype('int')
+df = joining_gl_codes(df, coa_mcc_df)
 
-# Joining coa mcc link table
-creditcard_df = pd.merge(creditcard_df,
-                         coa_mcc_df,
-                         how='left',
-                         on='MCC')
-
-# missing gl check
-if missing_gl_check(creditcard_df) != 0:
+if missing_gl_check(df) != 0:
     print("GL_Codes are missing, need to update the coa and mcc link table")
-    missing_mcc(creditcard_df)
+    missing_mcc(df)
 else:
     print("No GL_Codes missing")
 
-creditcard_df['Description'] = creditcard_df['MCC_Description'] + \
-    ': ' + creditcard_df['Name']
-creditcard_df = creditcard_df[['Date', 'GL_Code',
-                               'Account', 'Description', 'DEBIT', 'CREDIT']]
-creditcard_df = creditcard_df.round(2)
+df = processing_df(df)
+credit_entries_df = creating_credit_entries(df)
+debit_entries_df = creating_debit_entries(df)
+df = pd.concat([df, credit_entries_df, debit_entries_df]
+               ).reset_index(drop=True)
+df = df.sort_values(by=['Order_Col', 'Sub_Order_Col']).reset_index(drop=True)
 
-# creating sort columns
-creditcard_df = creditcard_df.sort_values(by='Date').reset_index(drop=True)
-creditcard_df['Order_Col'] = creditcard_df.index + 1
-creditcard_df['Sub_Order_Col'] = np.where(
-    creditcard_df['DEBIT'].isnull(), 2, 1)
-
-# Creating credit entries
-credit_entries = creditcard_df[creditcard_df['CREDIT'].isnull()].reset_index(
-    drop=True).copy()
-credit_entries['GL_Code'] = 200101
-credit_entries['Account'] = 'EdwardJones MasterCard'
-credit_entries['Sub_Order_Col'] = 2
-credit_entries = credit_entries.rename(
-    columns={'DEBIT': 'CREDIT', 'CREDIT': 'DEBIT'})
-credit_entries = credit_entries[['Date', 'GL_Code', 'Account',
-                                 'Description', 'DEBIT', 'CREDIT', 'Order_Col', 'Sub_Order_Col']]
-
-# Creating debit entries
-debit_entries = creditcard_df[creditcard_df['DEBIT'].isnull()].reset_index(
-    drop=True).copy()
-debit_entries['GL_Code'] = 200101
-debit_entries['Account'] = 'EdwardJones MasterCard'
-debit_entries['Sub_Order_Col'] = 1
-debit_entries = debit_entries.rename(
-    columns={'DEBIT': 'CREDIT', 'CREDIT': 'DEBIT'})
-debit_entries = debit_entries[['Date', 'GL_Code', 'Account',
-                               'Description', 'DEBIT', 'CREDIT', 'Order_Col', 'Sub_Order_Col']]
-
-# Appending debit and credit entries
-creditcard_df = pd.concat(
-    [creditcard_df, credit_entries, debit_entries]).reset_index(drop=True)
-
-creditcard_df = creditcard_df.sort_values(
-    by=['Order_Col', 'Sub_Order_Col']).reset_index(drop=True)
-
-# debits = credits check
-if debit_credit_check(creditcard_df) == True:
+if debit_credit_check(df) == True:
     print("Debits equal credits. Proceed to next phase")
 else:
     print("Something went wrong")
 
-creditcard_df['Month_Num'] = creditcard_df['Date'].dt.month
-creditcard_df['Transaction_ID'] = 'cc-' + creditcard_df['Month_Num'].astype(
-    "str") + '-' + (creditcard_df.index + 1).astype("str")
-
-creditcard_df = pd.merge(creditcard_df,
-                         month_df,
-                         on='Month_Num',
-                         how='left')
-
-months = creditcard_df['Month'].unique()
-file_path = os.path.join(cwd, config.get(
-    "data_outputs_directory", "JOURNAL_ENTRIES"))
-month_num = 1
-for month in months:
-    df = creditcard_df[creditcard_df['Month'] == month]
-    df = df[['Transaction_ID', 'Date', 'GL_Code',
-             'Account', 'Description', 'DEBIT', 'CREDIT']]
-    file_name = f'{month_num}_creditcard_{month}_entries.csv'
-    df.to_csv(os.path.join(file_path, file_name), index=False)
-    month_num += 1
+type = 'creditcard'
+creating_output(df, month_df, type, config, cwd)
