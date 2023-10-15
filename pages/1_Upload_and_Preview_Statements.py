5d59e64d1cd57f6b7adcaca0473895c0ff398bd6
tascoma <scomatony@outlook.com>
2023-10-15 14:44:14 -0500

    Added notes to script

--- pages/1_Upload_and_Preview_Statements.py
+++ pages/1_Upload_and_Preview_Statements.py
@@ -2,9 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 
-
+# Set up the page title
 st.title("Upload and Preview Statements")
 
+# Allow the user to upload a file
 file = st.file_uploader("Upload Bank Statement, Credit Card, or Paystub")
 
 if file:
@@ -21,6 +22,7 @@ if file:
 uploaded_files = os.listdir("uploads")
 
 if uploaded_files:
+    # If there are uploaded files, display them in a dropdown menu
     st.subheader("Uploaded Files")
     selected_file = st.selectbox("Select a file", uploaded_files)
 
@@ -29,22 +31,28 @@ if uploaded_files:
 
     # Preview the selected file based on the file extension
     if file_extension == ".csv":
+        # If the file is a CSV, display a preview of the data
         df = pd.read_csv(os.path.join("uploads", selected_file))
         st.subheader("Preview of the CSV file")
         st.dataframe(df)
     elif file_extension == ".xlsx":
+        # If the file is an Excel file, display a preview of the data
         df = pd.read_excel(os.path.join("uploads", selected_file))
         st.subheader("Preview of the Excel file")
         st.dataframe(df)
     elif file_extension == ".pdf":
+        # If the file is a PDF, display a message that previews are unavailable
         st.subheader("Preview Unavailable")
     else:
+        # If the file is not a CSV, Excel, or PDF, display a message that previews are unavailable
         st.subheader("Preview Unavailable")
     
+    # Allow the user to remove the selected file
     if st.button("Remove File"):
         file_to_remove = os.path.join("uploads", selected_file)
         os.remove(file_to_remove)
         st.success(f"{selected_file} has been removed.")
 
 else:
+    # If there are no uploaded files, display a message prompting the user to add files
     st.header("Add Files to Preview")
