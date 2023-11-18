5b200f2fccd93bb7ecf940e8ca85bcdd0b99a2ee
tascoma <scomatony@outlook.com>
2023-11-18 13:26:48 -0600

    Fix email validation and add flash messages

--- webapp/auth.py
+++ webapp/auth.py
@@ -23,19 +23,14 @@ def sign_up():
         password2 = request.form.get('password2')
 
         if len(email) < 4:
-            # flash('Email must be greater than 3 characters.', category='error')
-            pass
+            flash('Email must be greater than 3 characters.', category='error')
         elif len(firstName) < 2:
-            # flash('First name must be greater than 1 character.', category='error')
-            pass
+            flash('First name must be greater than 1 character.', category='error')
         elif password1 != password2:
-            # flash('Passwords don\'t match.', category='error')
-            pass
+            flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
-            # flash('Password must be at least 7 characters.', category='error')
-            pass
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            # flash('Account created!', category='success')
-            pass
+            flash('Account created!', category='success')
 
     return render_template('sign_up.html')
