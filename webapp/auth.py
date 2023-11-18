4313242cd347e69b49e9bf6f70191696d7798bb3
tascoma <scomatony@outlook.com>
2023-11-18 09:10:06 -0600

    Add login and sign-up functionality to auth.py, and update views.py with new routes

--- webapp/auth.py
+++ webapp/auth.py
@@ -1,12 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html')
 
 
 @auth.route('/logout')
@@ -14,6 +14,28 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            # flash('Email must be greater than 3 characters.', category='error')
+            pass
+        elif len(firstName) < 2:
+            # flash('First name must be greater than 1 character.', category='error')
+            pass
+        elif password1 != password2:
+            # flash('Passwords don\'t match.', category='error')
+            pass
+        elif len(password1) < 7:
+            # flash('Password must be at least 7 characters.', category='error')
+            pass
+        else:
+            # flash('Account created!', category='success')
+            pass
+
+    return render_template('sign_up.html')
--- webapp/views.py
+++ webapp/views.py
@@ -1,8 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("home.html")
+
+@views.route('/process-statements')
+def process_statements():
+    return render_template("process_statements.html")
+
+@views.route('/view-statements')
+def view_statements():
+    return render_template("view_statements.html")
+
+@views.route('/view-data')
+def view_data():
+    return render_template("view_data.html")
+
+@views.route('/settings')
+def settings():
+    return render_template("settings.html")
