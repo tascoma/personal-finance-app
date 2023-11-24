b1b2ae55b095d6b38904e1f45d810d4d3b0dbdb3
tascoma <scomatony@outlook.com>
2023-11-24 11:26:27 -0600

    Added view data functionality

--- webapp/views.py
+++ webapp/views.py
@@ -3,7 +3,6 @@ from flask_login import login_required, current_user
 from . import db
 import pandas as pd
 
-
 views = Blueprint('views', __name__)
 
 @views.route('/')
@@ -11,45 +10,43 @@ views = Blueprint('views', __name__)
 def home():
     return render_template("home.html", user=current_user)
 
+
 @views.route('/process-statements')
 @login_required
 def process_statements():
     return render_template("process_statements.html", user=current_user)
+
 
 @views.route('/view-statements')
 @login_required
 def view_statements():
     return render_template("view_statements.html", user=current_user)
 
+
 @views.route('/manage-data', methods=['GET', 'POST'])
 @login_required
 def manage_data():
     table_names = db.metadata.tables.keys()
-    selected_table = False
-    columns = []
-    rows = []
-
     if request.method == 'POST':
         table_name = request.form.get('table')
-        selected_table = db.metadata.tables.get(table_name)
-        
-        if selected_table:
-            df = pd.read_sql_table(table_name, db.engine)
-            columns = list(df.columns)
-            rows = df.values.tolist()
-        
-        elif 'add' in request.form:
-            print("Adding data to table")
-            # Logic to add new data to the selected table
-            
-        elif 'edit' in request.form:
-            print("Editing data in table")
-            # Logic to edit data in the selected table
+        if table_name:
+            return redirect(url_for('views.view_data', table_name=table_name))
+    return render_template("manage_data.html", user=current_user, table_names=table_names)
 
-        # Handle delete logic similarly
-        
-    return render_template("manage_data.html", user=current_user, table_names=table_names, selected_table=selected_table, columns=columns, rows=rows)
 
+@views.route('/manage-data/view-data/<table_name>', methods=['GET', 'POST'])
+@login_required
+def view_data(table_name):
+    table_names = db.metadata.tables.keys()
+    df = pd.read_sql_table(table_name, db.engine)
+    column_names = list(df.columns)
+    rows = df.values.tolist()
+    if request.method == 'POST':
+        if 'table' in request.form:
+            table_name = request.form.get('table')
+            if table_name:
+                return redirect(url_for('views.view_data', table_name=table_name))
+    return render_template("manage_data.html", user=current_user, table_names=table_names, table_name=table_name, column_names=column_names, rows=rows)
 
 
 @views.route('/settings')
